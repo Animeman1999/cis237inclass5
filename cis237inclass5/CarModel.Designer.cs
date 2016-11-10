b51d6ecc0b346c0e2e54dbf638fc048b8c547b63
Jeffrey Martin <animeman1999@google.com>
2016-11-10 17:44:56 -0500

    Adds exampls of quering the database

--- cis237inclass5/CarModel.Designer.cs
+++ cis237inclass5/CarModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'O:\Advanced C#CIS 237\cis237inclass5\cis237inclass5\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Classes\Advanced C\cis237inclass5\cis237inclass5\CarModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- cis237inclass5/Program.cs
+++ cis237inclass5/Program.cs
@@ -10,6 +10,68 @@ namespace cis237inclass5
     {
         static void Main(string[] args)
         {
+            //Make a new instance of the entites class
+            CarsJMartinEntities carsJMartinEntites = new CarsJMartinEntities();
+
+            //***********************************
+            //List out all of the ars in the table
+            //**********************************
+            Console.WriteLine("Print the list");
+            foreach(Car car in carsJMartinEntites.Cars)
+            {
+                Console.WriteLine(car.id + " " + car.make + " " + car.model);
+            }
+
+            //*******************************************
+            //Find a specific one by the primary key
+            //******************************************
+
+            // PUll out a car from the table base on the id which is the primay key
+            //If the record doesn't exist in the database, it will return null, so
+            //check what you get back and see if it is null. If so, it doesn't exist
+
+            Car foundCar = carsJMartinEntites.Cars.Find("V0LCD1814"); //Find only finds by the primary key
+
+            //Print it out
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Print out a found car using the Find Method");
+            Console.WriteLine(foundCar.id + " " + foundCar.make + " " + foundCar.model);
+
+            //******************************************************************************
+            //Find a specific one by any property
+            //******************************************************************************
+
+            //Call the Where method on the table Cars and pass in a lamda expression
+            //for the criteria we are lookin for.  There is nothing special about the 
+            //word car in the part that reads : cart +> car.id == "V)...." It could be
+            //any characters we want it to be. It is just a variable name for the current
+            //car we are considering in the expression. This will automagically loop
+            //through all the Cars, and run the expression against each of them.  When
+            //The result is finnaly true, it will return car.
+
+            Car carToFind = carsJMartinEntites.Cars.Where(
+                car => car.model == "Challenger").First();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Print out a found car using the where Method");
+            Console.WriteLine(carToFind.id + " " + carToFind.make + " " + carToFind.model);
+
+            //****************************************************************************
+            //Get out multiple cars
+            //***************************************************************************
+            List<Car> queryCars = carsJMartinEntites.Cars.Where(
+                car => car.cylinders == 8
+                ).ToList();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Fincall all cars with 8 cylinders");
+
+            foreach(Car car in queryCars)
+            {
+                Console.WriteLine(car.id + " " + car.make + " " + car.model);
+            }
         }
     }
 }
